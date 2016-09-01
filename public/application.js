800a441fb15d4e8a64365bd2356d30d486f059b3
andela-mfalade <mayowa.falade@andela.com>
2016-09-01 16:31:12 +0100

    Update readme

--- public/application.js
+++ public/application.js
@@ -4,6 +4,6 @@ angular.module('youtubeDownloader', [
   'youtubeDownloader.controller',
   'youtubeDownloader.service']
 ).value('options', {
-  // 'API_URL': 'https://youtubetomp3ly.herokuapp.com/api/v1'
-  'API_URL': 'http://localhost:5000/api/v1'
+  'API_URL': 'https://youtubetomp3ly.herokuapp.com/api/v1'
+  // 'API_URL': 'http://localhost:5000/api/v1'
 });
